0991f83141e3b601f06a4ca7e1850b44e392e648
Gio Lodi <gio@mokacoding.com>
2024-11-07 19:48:11 +1100

    More debug info

--- .buildkite/release-mac.sh
+++ .buildkite/release-mac.sh
@@ -6,5 +6,12 @@ brew install go
 echo "--- :bug: Print debug info"
 which go
 
+echo "--- :bug: Print debug info"
+echo "$PATH"
+
+echo "--- :wrench: Add go to path (just in case)"
+PATH=$PATH:$(go env GOPATH)/bin
+export PATH
+
 echo "--- :package: Packaging for macOS"
 make release-mac
