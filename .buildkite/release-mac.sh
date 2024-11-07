0049bc4b6a6d06717363a901a67d30eda54d6af7
Gio Lodi <gio@mokacoding.com>
2024-11-07 21:08:05 +1100

    Try `colema delete`

--- .buildkite/release-mac.sh
+++ .buildkite/release-mac.sh
@@ -14,11 +14,18 @@ echo "~~~ Install Docker"
 brew install docker
 echo "~~~ Install colima"
 brew install colima
-echo "~~~ Start colima"
+echo "~~~ [Workaround attempt] Delete colima settings"
 # HOMEBREW_PREFIX results unbound in CI
 # https://buildkite.com/automattic/download/builds/17#0193060c-6559-4a6c-a5c0-4074a2ec7686/470-471
 # "$HOMEBREW_PREFIX/opt/colima/bin/colima" start --runtime docker
 # When can hardcode the Apple Silicon path, however, because we know our CI at this time only runs on Apple Silicon
+#
+# See failure at
+# https://buildkite.com/automattic/download/builds/18#01930613-abac-4673-bee5-51d94d1c31fd
+#
+# And see https://buildkite.com/automattic/download/builds/18#01930613-abac-4673-bee5-51d94d1c31fd for why we call delete first
+/opt/homebrew/opt/colima/bin/colima delete --force
+echo "~~~ Start colima"
 /opt/homebrew/opt/colima/bin/colima start --runtime docker
 echo "~~~ Verify Docker setup"
 docker version
