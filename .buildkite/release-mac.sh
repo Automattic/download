577a7fd87fe2cb203b2b32443c7517b40465cbb3
Gio Lodi <gio@mokacoding.com>
2024-11-07 21:02:36 +1100

    Remove HOMEBREW env var

--- .buildkite/release-mac.sh
+++ .buildkite/release-mac.sh
@@ -14,10 +14,12 @@ echo "~~~ Install Docker"
 brew install docker
 echo "~~~ Install colima"
 brew install colima
-# echo "~~~ Verify colima permissions"
-# ls -la "$HOME/.colima"
 echo "~~~ Start colima"
-"$HOMEBREW_PREFIX/opt/colima/bin/colima" start --runtime docker
+# HOMEBREW_PREFIX results unbound in CI
+# https://buildkite.com/automattic/download/builds/17#0193060c-6559-4a6c-a5c0-4074a2ec7686/470-471
+# "$HOMEBREW_PREFIX/opt/colima/bin/colima" start --runtime docker
+# When can hardcode the Apple Silicon path, however, because we know our CI at this time only runs on Apple Silicon
+/opt/homebrew/opt/colima/bin/colima start --runtime docker
 echo "~~~ Verify Docker setup"
 docker version
 docker ps
