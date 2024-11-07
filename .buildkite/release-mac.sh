324e08a3a9c89f9e1c7bc10865ac3b518fa5a3d7
Gio Lodi <gio@mokacoding.com>
2024-11-07 19:53:40 +1100

    Set up Docker

--- .buildkite/release-mac.sh
+++ .buildkite/release-mac.sh
@@ -1,17 +1,24 @@
 #!/bin/bash -eu
 
 echo "--- :go: Installing Go"
+echo "~~~ Install Go"
 brew install go
-
-echo "--- :bug: Print debug info"
-which go
-
-echo "--- :bug: Print debug info"
-echo "$PATH"
-
-echo "--- :wrench: Add go to path (just in case)"
+echo "~~~ Add Go to PATH"
+echo "PATH before: $PATH"
 PATH=$PATH:$(go env GOPATH)/bin
 export PATH
+echo "PATH after: $PATH"
+
+echo "--- :docker: Installing Docker"
+echo "~~~ Install Docker"
+brew install docker
+echo "~~~ Install colima"
+brew install colima
+echo "~~~ Start"
+colima start --runtime docker
+echo "~~~ Verify setup"
+docker version
+docker ps
 
 echo "--- :package: Packaging for macOS"
 make release-mac
