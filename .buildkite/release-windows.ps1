dce527c1d334d78100f983b87cf3ebb228bcae40
Gio Lodi <gio@mokacoding.com>
2024-11-15 11:45:38 +1100

    Add Buildkite pipeline to build exe and appx

---
+++ .buildkite/release-windows.ps1
@@ -0,0 +1,58 @@
+# Stop script execution when a non-terminating error occurs
+$ErrorActionPreference = "Stop"
+
+& "prepare_windows_host_for_node.ps1"
+
+Write-Host "--- :windows: Installing make"
+choco install make
+
+Write-Host "--- :windows: Installing Go"
+# The install process requires user confirmation, hence the -y option
+# See https://buildkite.com/automattic/download/builds/64#01931dc8-2a8f-4d2b-8382-8b69964896c5/240-261
+choco install go -y
+
+refreshenv
+
+Write-Host "--- :windows: Installing GCC"
+# The install process requires user confirmation, hence the -y option
+# See https://buildkite.com/automattic/download/builds/64#01931dc8-2a8f-4d2b-8382-8b69964896c5/240-261
+choco install mingw -y
+
+refreshenv
+
+# # Using Podman instead of Docker because apparently installing Docker requires a restart
+# # See https://community.chocolatey.org/packages/docker-desktop
+# # TODO: Are there other options than docker-desktop?
+# Write-Host "--- :windows: Installing Podman"
+# # The install process requires user confirmation, hence the -y option
+# # See https://buildkite.com/automattic/download/builds/64#01931dc8-2a8f-4d2b-8382-8b69964896c5/240-261
+# choco install podman-cli -y
+
+# refreshenv
+
+# podman machine init
+# podman machine start
+
+# Write-Host "--- :windows: Installing Docker"
+# choco install docker-cli -y
+# choco install docker-desktop -y
+
+# Write-Host "--- :chocolate: Print logs"
+# cat C:\ProgramData\chocolatey\logs\chocolatey.log
+
+# refreshenv
+
+# Write-Host "--- :bug: Print help"
+# C:\ProgramData\chocolatey\lib\docker-cli --help
+
+# This should avoid failures like
+# > no matching manifest for windows/amd64 10.0.17763 in the manifest list entries
+# See https://buildkite.com/automattic/download/builds/75#01931e2b-2bdf-40b3-8c4f-d8f2f7c6bd15/317-357
+# Write-Host "--- :docker: Switch to Linux containers"
+# Write-Host "+++ TODO"
+
+& "$PSScriptRoot\install-windows-10-sdk.ps1"
+If ($LastExitCode -ne 0) { Exit $LastExitCode }
+
+make release-windows
+if ($LastExitCode -ne 0) { Exit $LastExitCode }
--- Makefile
+++ Makefile
@@ -1,9 +1,20 @@
-BUILD_VERSION=1
-BUILD_TIME=$(shell date +%s) 
+# Windows apps have upper bounds on the version components:
+#
+# For Windows 10 or Windows 11 (UWP) packages, the last (fourth) section of the
+# version number is reserved for Store use and must be left as 0 when you build
+# your package (although the Store may change the value in this section). The
+# other sections must be set to an integer between 0 and 65535 (except for the
+# first section, which cannot be 0).
+#
+# https://learn.microsoft.com/en-us/windows/apps/publish/publish-your-app/msix/app-package-requirements#package-version-numbering
+BUILD_VERSION=1.$(shell date +%y%m)
+BUILD_TIME=$(shell date +%d%H)
+APP_ID=com.automattic.download
 
 all: release
 
 fyne:
+	@echo "--- :go: Installing Go tools"
 	go install github.com/fyne-io/fyne-cross@latest
 	go install fyne.io/fyne/v2/cmd/fyne@v2.5
 
@@ -17,3 +28,24 @@ release:
 	mv fyne-cross/dist/windows-amd64/download.exe.zip fyne-cross/dist/download-windows-amd64.zip
 	mv fyne-cross/dist/windows-386/download.exe.zip fyne-cross/dist/download-windows-i386.zip
 	rm -rf fyne-cross/bin fyne-cross/tmp fyne-cross/dist/darwin-amd64 fyne-cross/dist/darwin-arm64 fyne-cross/dist/windows-arm64 fyne-cross/dist/windows-amd64 fyne-cross/dist/windows-386
+
+# TODO: Find a way to DRY the -app* flags?
+#
+# Notice -appBuild 1: Windows docs says this should be 0 for store use but fyne requires it to be > 0
+#
+# Also notice that this requires having the code signing cert available locally (certificat.pfx)
+# and therefore only works in our Windows CI.
+release-windows: fyne
+	@echo "--- :package: Packaging"
+	fyne package \
+		-appID $(APP_ID) \
+		-appVersion $(BUILD_VERSION).$(BUILD_TIME) \
+		-appBuild 1
+	@echo "--- :rocket: Preparing package for release"
+	fyne release \
+		-appID $(APP_ID) \
+		-appVersion $(BUILD_VERSION).$(BUILD_TIME) \
+		-appBuild 1 \
+		-developer 'CN="Automattic, Inc.", O="Automattic, Inc.", S=California, C=US' \
+		-certificate certificate.pfx \
+		-password $(WINDOWS_CODE_SIGNING_CERT_PASSWORD)
